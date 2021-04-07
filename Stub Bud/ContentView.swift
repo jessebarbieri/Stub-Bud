f9364853aaca47a0d47114c41b0060c43fcf01b5
Jesse Barbieri <barbierijesse@gmail.com>
2021-04-07 11:27:37 -0400

    Learned VStack and replaced the prior ZStack, replaced original state array with a dictionary. Removed originally set positions. Declare middle constant (currently unused).

--- Stub Bud/ContentView.swift
+++ Stub Bud/ContentView.swift
@@ -7,9 +7,9 @@
 
 import SwiftUI
 
-var state_picker = ["Alabama", "Alaska", "Arizona", "Arkansas","California","Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine" , "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada","New Jersey","New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-
 var state_dict = ["Alabama":"AL", "Alaska":"AK", "Arizona": "AZ", "Arkansas": "AR","California": "CA","Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME" , "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV","New Jersey": "NJ","New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY"]
+
+let middle = UIScreen.main.bounds.size.width/2
 
 
 /*
@@ -19,23 +19,22 @@ struct ContentView: View {
     @State private var selected_state = "New Jersey"
     @State var isModal: Bool = false
     var body: some View {
-            ZStack{
-                Color(.white).ignoresSafeArea()
-                Text("Select Your State").font(.largeTitle).fontWeight(.bold).padding().foregroundColor(Color(.black)).position(x: UIScreen.main.bounds.size.width/2, y:UIScreen.main.bounds.size.height/8)
+            VStack{
+                Text("Select Your State").font(.largeTitle).fontWeight(.bold).padding().foregroundColor(Color(.black))
                 Picker("State", selection: $selected_state){
-                    ForEach(state_picker, id:\.self){
+                    ForEach(state_dict.keys.sorted(), id:\.self){
                     Text($0)
                         .foregroundColor(Color(.black))
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 }
-            }.position(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height-275)
-            Image(getState(state: selected_state)).resizable().frame(width: 225, height: 225, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).position(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2.75)
+            }
+            Image(getState(state: selected_state)).resizable().frame(width: 225, height: 225, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             
                 Button("Next"){
                     self.isModal = true
                 }.sheet(isPresented: $isModal, content: {
                     SecondView()
-                }).position(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height-125).foregroundColor(.black).font(.title2)
+                }).foregroundColor(.black).font(.title2).padding(.top, 50.0)
             }
     }
 }
@@ -59,3 +58,9 @@ func getState(state: String) -> String {
 }
 
 
+/*
+ .position(x: middle, y:UIScreen.main.bounds.size.height/8)
+ .position(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height-275)
+ .position(x: middle, y: UIScreen.main.bounds.size.height/2.75)
+ .position(x: middle, y: UIScreen.main.bounds.size.height-125).foregroundColor(.black).font(.title2)
+ */
