9034dc8d23aea808ccc975e1f141392e1ae922c8
Jesse Barbieri <barbierijesse@gmail.com>
2021-04-08 19:24:15 -0400

    Adding options to make proper tax calculation. Using various pickers, creating the layout vertically and horizontally depending on space.

--- Stub Bud/SecondView.swift
+++ Stub Bud/SecondView.swift
@@ -10,14 +10,45 @@ import SwiftUI
 struct SecondView: View {
     @Binding var selected_state: String
     let abrev: String
+    let middle = UIScreen.main.bounds.size.width/2
+    var marital_array = ["Single", "Married"]
+    var rate_array = ["Salary", "Hourly"]
+    @State var marital_status = 0
+    @State var pay_rate = 1
+    @State var pay_frequency = "Bi-Weekly"
     var body: some View {
+    VStack{
         HStack{
-            Image(abrev).resizable().frame(width: 100, height: 100, alignment: .center).padding(.bottom, 25.0)
-            Text(selected_state)
+            Image(abrev).resizable().frame(width: 75, height: 75, alignment: .top).padding(.bottom, 25.0)
+            Text(selected_state).font(.title3).fontWeight(.bold)
+            Image(abrev).resizable().frame(width: 75, height: 75, alignment: .top).padding(.bottom, 25.0)
         }
         
+        Text("Let's gather some information to make our estimation. Don't worry, we don't store any of your data.")
+        HStack{
+            Picker(selection: $marital_status, label: Text("Marital Status")) {
+                ForEach(0 ..< marital_array.count){
+                    Text(self.marital_array[$0]).tag($0)
+                }
+            }.pickerStyle(SegmentedPickerStyle()).frame(width: 150, height: 50, alignment: .center)
+            Picker(selection: $pay_rate, label: Text("Marital Status")) {
+                ForEach(0 ..< rate_array.count){
+                    Text(self.rate_array[$0]).tag($0)
+                }
+            }.pickerStyle(SegmentedPickerStyle()).frame(width: 150, height: 50, alignment: .center)
+        }
+        
+        Picker(selection: $pay_frequency, label: Text("Pay Frequency"), content: {
+                        Text("Weekly").tag(1)
+                        Text("Bi-Weekly").tag(2)
+                        Text("Semi-Monthly").tag(3)
+                        Text("Monthly").tag(4)
+                        Text("Annually").tag(5)
+        }).scaleEffect(0.75).frame(width: 250, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center).padding(.bottom, 50)
+        }
     }
 }
+
 
 
 struct SecondView_Previews: PreviewProvider {
